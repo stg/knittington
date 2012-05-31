b4c3eb4cae15690bff787ba264aaf514dd06e913
Thor Grimner <senseitg@hotmail.com>
2012-05-31 14:43:15 +0200

    icon for windows gui app

--- win-gui-make.bat
+++ win-gui-make.bat
@@ -1,7 +1,9 @@
 @echo off
+echo [res\windows.res] building resources...
+windres res/windows.rc -O coff -o res/windows.res
 echo [bin-win\gui_sdl12.exe] building sdl1.2 gui executable...
-gcc -g gui/*.c lib/src/*.c -I lib/include/ -mwindows -lmingw32 -lsdlmain -lsdl -lsetupapi -o bin-win\gui_sdl12.exe
+gcc -g gui/*.c lib/src/*.c res/windows.res -I lib/include/ -mwindows -lmingw32 -lsdlmain -lsdl -lsetupapi -o bin-win\gui_sdl12.exe
 strip bin-win\gui_sdl12.exe
 echo [bin-win\gui.exe] building sdl2.0 gui executable...
-gcc -g gui/*.c lib/src/*.c -I lib/include/ -mwindows -lwinmm -lmingw32 -lsdl2main -lsdl2 -lsdl2.dll -lsetupapi -DSDL2 -lfreeimage -o bin-win\gui.exe
+gcc -g gui/*.c lib/src/*.c res/windows.res -I lib/include/ -mwindows -lwinmm -lmingw32 -lsdl2main -lsdl2 -lsdl2.dll -lsetupapi -DSDL2 -lfreeimage -o bin-win\gui.exe
 strip bin-win\gui.exe
