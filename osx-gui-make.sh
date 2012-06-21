aefee040d8bf50712d38ad11ba477a13c2bfc383
Jonas Bengtsson <jonas.b@gmail.com>
2012-06-21 23:34:09 +0200

    kh940knit -> Knittington

--- osx-gui-make.sh
+++ osx-gui-make.sh
@@ -4,5 +4,5 @@ echo [bin-osx/gui] building command-line-interface binary...
 gcc gui/*.c lib/src/*.c -framework Cocoa -I lib/include/ -lfreeimage -lstdc++ -lSDLmain -lSDL -o bin-osx/gui_sdl12
 
 echo [bin-osx/gui] building SDL2 gui binary...
-gcc gui/*.c lib/src/*.c -framework Cocoa -Fbin-osx/kh940knit.app/Contents/Frameworks -framework SDL2 -I lib/include/ -lfreeimage -lstdc++ -DSDL2 -o bin-osx/kh940knit.app/Contents/MacOS/kh940knit
-install_name_tool -change @rpath/SDL2.framework/Versions/A/SDL2 @executable_path/../Frameworks/SDL2.framework/Versions/A/SDL2 bin-osx/kh940knit.app/Contents/MacOS/kh940knit
+gcc gui/*.c lib/src/*.c -framework Cocoa -Fbin-osx/Knittington.app/Contents/Frameworks -framework SDL2 -I lib/include/ -lfreeimage -lstdc++ -DSDL2 -o bin-osx/Knittington.app/Contents/MacOS/Knittington
+install_name_tool -change @rpath/SDL2.framework/Versions/A/SDL2 @executable_path/../Frameworks/SDL2.framework/Versions/A/SDL2 bin-osx/Knittington.app/Contents/MacOS/Knittington
